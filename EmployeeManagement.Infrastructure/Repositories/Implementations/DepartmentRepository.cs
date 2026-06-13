79ac24171229f43973d8c69c0cffcc0ac030afb1
Ammar Yasser <ammar.yasser8920@gmail.com>
2026-06-13 08:48:56 +0300

    Add Department repository implementation

---
+++ EmployeeManagement.Infrastructure/Repositories/Implementations/DepartmentRepository.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagement.Domain.Entities;
+using EmployeeManagement.Infrastructure.Data;
+using EmployeeManagement.Infrastructure.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Infrastructure.Repositories.Implementations
+{
+    public class DepartmentRepository : GenericRepository<Department>, IDepartmentRepository
+    {
+        public DepartmentRepository(AppDbContext context) :base(context)
+        {
+            
+        }
+    }
+}
