db5381b02d2d6d7ba4f522263142750f992b8b60
Ammar Yasser <ammar.yasser8920@gmail.com>
2026-06-13 08:44:06 +0300

    Add Employee repository implementation

--- EmployeeManagement.Infrastructure/Repositories/Implementations/EmployeeRepository.cs
+++ EmployeeManagement.Infrastructure/Repositories/Implementations/EmployeeRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using EmployeeManagement.Domain.Entities;
+using EmployeeManagement.Infrastructure.Data;
+using EmployeeManagement.Infrastructure.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,36 @@ using System.Threading.Tasks;
 
 namespace EmployeeManagement.Infrastructure.Repositories.Implementations
 {
-    internal class EmployeeRepository
+    public class EmployeeRepository : GenericRepository<Employee>, IEmployeeRepository
     {
+        public EmployeeRepository(AppDbContext context) : base(context)
+        {
+            
+        }
+        public async Task<IEnumerable<Employee>> GetAllWithDepartmentAsync()
+        {
+            return await _context.Employees.Include(e => e.Department)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<Employee?> GetByIdWithDepartmentAsync(int id)
+        {
+            return await _context.Employees.Include(e => e.Department)
+                .FirstOrDefaultAsync(e => e.Id == id);
+        }
+
+        public async Task<IEnumerable<Employee>> SearchAsync(string keyword)
+        {
+            keyword = keyword.Trim().ToLower();
+            return await _context.Employees
+               .Include(e => e.Department)
+               .Where(e =>
+                   e.FullName.Contains(keyword) ||
+                   e.Department!.Name.Contains(keyword))
+               .AsNoTracking()
+               .ToListAsync();
+
+        }
     }
 }
