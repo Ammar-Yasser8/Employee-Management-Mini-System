de0552e14605a365a4d384629d77ca201a90786a
Ammar Yasser <ammar.yasser8920@gmail.com>
2026-06-13 16:35:32 +0300

    Add depandancies to program.cs

--- EmployeeP_Management.API/Program.cs
+++ EmployeeP_Management.API/Program.cs
@@ -1,3 +1,10 @@
+using EmployeeManagement.Application.Services.Implementations;
+using EmployeeManagement.Application.Services.Interfaces;
+using EmployeeManagement.Infrastructure.Data;
+using EmployeeManagement.Infrastructure.Repositories.Implementations;
+using EmployeeManagement.Infrastructure.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>
+    (options =>options.UseSqlServer
+    (builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IDepartmentService, DepartmentService>();
 
 var app = builder.Build();
 
