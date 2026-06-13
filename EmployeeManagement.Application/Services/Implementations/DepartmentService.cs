97c6da81c5e6b1c6f20bfa0908da61e2176ebc41
Ammar Yasser <ammar.yasser8920@gmail.com>
2026-06-13 16:10:01 +0300

    Implement department service

---
+++ EmployeeManagement.Application/Services/Implementations/DepartmentService.cs
@@ -0,0 +1,76 @@
+﻿using EmployeeManagement.Application.DTOs;
+using EmployeeManagement.Application.Services.Interfaces;
+using EmployeeManagement.Domain.Entities;
+using EmployeeManagement.Infrastructure.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Application.Services.Implementations
+{
+    public class DepartmentService : IDepartmentService
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public DepartmentService(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+
+        public async Task CreateAsync(CreateDepartmentDto dto)
+        {
+            var department = new Department
+            {
+                Name = dto.Name
+            };
+            await _departmentRepository.AddAsync(department);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var department = await _departmentRepository.GetByIdAsync(id);
+            if (department == null)
+                throw new KeyNotFoundException("Department not found");
+
+            await _departmentRepository.DeleteAsync(department);
+
+        }
+
+        public async Task<IEnumerable<DepartmentResponseDto>> GetAllAsync()
+        {
+            var depatments = await _departmentRepository.GetAllAsync();
+            return depatments.Select(MapToDto);
+        }
+
+        public async Task<DepartmentResponseDto?> GetByIdAsync(int id)
+        {
+            var department =await _departmentRepository.GetByIdAsync(id);
+
+            if (department == null)
+                return null;
+
+            return MapToDto(department);
+        }
+
+        public async Task UpdateAsync(int id, UpdateDepartmentDto dto)
+        {
+            var department = await _departmentRepository.GetByIdAsync(id);
+            if(department is null)
+            {
+                throw new Exception($"Department with id {id} not found.");
+            }
+            department.Name = dto.Name;
+            await _departmentRepository.UpdateAsync(department);
+        }
+        private static DepartmentResponseDto MapToDto(Department department)
+        {
+            return new DepartmentResponseDto
+            {
+                Id = department.Id,
+                Name = department.Name
+            };
+        }
+    }
+}
---
+++ EmployeeManagement.Application/Services/Interfaces/IDepartmentService.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeManagement.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Application.Services.Interfaces
+{
+    public interface IDepartmentService
+    {
+        public Task<IEnumerable<DepartmentResponseDto>> GetAllAsync();
+        public Task<DepartmentResponseDto?> GetByIdAsync(int id);
+        public Task CreateAsync(CreateDepartmentDto dto);
+        public Task UpdateAsync(int id, UpdateDepartmentDto dto);
+        public Task DeleteAsync(int id);
+    }
+}
