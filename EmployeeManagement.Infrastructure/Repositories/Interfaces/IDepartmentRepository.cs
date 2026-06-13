514dd91ba2c86823d6e8acefea71c3089dddfe47
Ammar Yasser <ammar.yasser8920@gmail.com>
2026-06-13 08:50:56 +0300

    Chnage access modifier from internal to public

--- EmployeeManagement.Infrastructure/Repositories/Interfaces/IDepartmentRepository.cs
+++ EmployeeManagement.Infrastructure/Repositories/Interfaces/IDepartmentRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeManagement.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace EmployeeManagement.Infrastructure.Repositories.Interfaces
 {
-    internal interface IDepartmentRepository
+    public interface IDepartmentRepository : IGenericRepository<Department>
     {
     }
 }
