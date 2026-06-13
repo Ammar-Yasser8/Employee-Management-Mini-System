4f194bb3082cb34c500804eabb7f3e4572bb1266
Ammar Yasser <ammar.yasser8920@gmail.com>
2026-06-13 07:22:46 +0300

    Implement GenericRepository methods

--- EmployeeManagement.Infrastructure/Repositories/Implementations/GenericRepository.cs
+++ EmployeeManagement.Infrastructure/Repositories/Implementations/GenericRepository.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using EmployeeManagement.Domain.Entities;
+using EmployeeManagement.Infrastructure.Data;
+using EmployeeManagement.Infrastructure.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EmployeeManagement.Infrastructure.Repositories.Implementations
 {
-    internal class GenericRepository
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
     {
+        protected readonly AppDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
     }
-}
+    }
+
